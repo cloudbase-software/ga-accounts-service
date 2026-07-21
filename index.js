7457148739594add7ccb6c3df9c877064ce0fdc1
tristan hall <ME\du_223463-1782724478@a-usztvd0pr371.me.ga.co>
2026-07-21 10:33:34 +0100

    added the code to start the app with annotations

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv'; // Dotenv is a package that lets us use environment variables
+dotenv.config(); // we call this to initialise dotenv
+import express from "express"; // We need express as it's the framework for building APIs
 
-const port = process.env.PORT;
+const port = process.env.PORT; // get the PORT variable from the .env file
+const app = express(); // create a new express application
 
-console.log(port);
+app.use(express.json()); // this configures express to read request body when a user sends data
+
+async function startServer() { // async functions can wait for things to happen (await)
+  try { // try to start the server using app.listen
+    app.listen(port, () => console.log(`🤖 Listening on Port: ${port}`));
+  } catch (err) { // if there's an error, tell us.
+    console.log("🤖 Oh no something went wrong", err);
+  }
+}
+
+startServer(); // call the start server function to start the app.
