693afe132e80ed6b6f2a2f405df11af4f0b36fe5
tristan hall <ME\du_223463-1782724478@a-usztvd0pr371.me.ga.co>
2026-07-21 11:30:02 +0100

    router imports

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import dotenv from 'dotenv'; // Dotenv is a package that lets us use environment variables
 dotenv.config(); // we call this to initialise dotenv
 import express from "express"; // We need express as it's the framework for building APIs
+import Router from "./views/router.js"; // imports the router object from the router file 
 
 const port = process.env.PORT; // get the PORT variable from the .env file
 const app = express(); // create a new express application
 
 app.use(express.json()); // this configures express to read request body when a user sends data
-
+app.use(Router); // tell the app to use the router for the functionality when a user sends a request
 
 async function startServer() { // async functions can wait for things to happen (await)
   try { // try to start the server using app.listen
